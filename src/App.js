55992937fd0b28008908d49598ff867ffc90ba0a
danielghandahari <dangha94@gmail.com>
2019-07-19 00:10:29 +0200

    made google maps component functional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
+import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+
 import logo from './logo.svg';
 import './App.css';
+import LocationSearchInput from './components/LocationSearchInput';
+
+// const query = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=59.336220,18.079210&type=subway_station&rankby=distance&key=AIzaSyCwZhKoccHJYFnxSQ5FPOsAqOYHJ7Kly2Y'
 
 function App() {
-  const [from, setFrom] = useState('tja');
+  const [address, setAddress] = useState('');
+
+  const handleChange = address => {
+    setAddress(address);
+  };
+
+  const handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log('Success', latLng))
+      .catch(error => console.error('Error', error));
+  };
 
   return (
     <div className="App">
-      <input type="text" value={from} onChange={e => setFrom(e.target.value)} />
-      <div>{from || ''}</div>
+      <LocationSearchInput
+        handleChange={handleChange}
+        handleSelect={handleSelect}
+        value={address}
+      />
     </div>
   );
 }
