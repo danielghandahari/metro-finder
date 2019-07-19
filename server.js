29e453896080336e5378db3bd348ddf6cde6378a
danielghandahari <dangha94@gmail.com>
2019-07-19 12:55:44 +0200

    added concurrently

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/api/place', (req, res) => res.send('places!'));
